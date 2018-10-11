2cd5bd8102d0afcc5fdd2c0234a9b21aa12b1ed0
Louie Kotler <louie9479@gmail.com>
2018-10-11 12:44:17 -0400

    Fix clicking issue

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 var state = 0;
-var a = 255;
 var r = 255;
 var g = 0;
 var b = 0;
@@ -10,42 +9,43 @@ function setup() {
 
 function draw() {
   if (mouseIsPressed) {
-    fill(0);
-  } else {
+  //  fill(0);
+  //} else {
 
-		if(state == 0){
+		if(state == 0) {
     g++;
-    if(g == 255)
+    	if(g == 255)
         state = 1;
-}
-if(state == 1){
+		}
+		if(state == 1) {
     r--;
-    if(r == 0)
-        state = 2;
-}
-if(state == 2){
+    	if(r == 0)
+				state = 2;
+		}
+		if(state == 2) {
     b++;
-    if(b == 255)
-        state = 3;
-}
-if(state == 3){
+    	if(b == 255)
+    		state = 3;
+		}
+		if(state == 3) {
     g--;
-    if(g == 0)
-        state = 4;
-}
-if(state == 4){
+    	if(g == 0)
+    		state = 4;
+		}
+		if(state == 4) {
     r++;
-    if(r == 255)
-        state = 5;
-}
-if(state == 5){
+    	if(r == 255)
+    		state = 5;
+		}
+		if(state == 5) {
     b--;
-    if(b == 0)
+    	if(b == 0)
         state = 0;
-}
+		}
 
 		fill(r, g, b);
 
-  }
+//  }
   ellipse(mouseX, mouseY, 80, 80);
+	}
 }
